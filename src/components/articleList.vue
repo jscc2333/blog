6c0e4e33699a479ba365c3b1611628938620b807
c <jscc2333@163.com>
2017-10-23 10:47:13 +0800

    feat:(view) complete the blog list design
    use mock data in components  articleList to archieve blog list
    design,build the basic frame for the sidebar

---
+++ src/components/articleList.vue
@@ -0,0 +1,137 @@
+<template>
+  <div class="article-list">
+    <article class="article">
+      <h2 class="title">测试用标题</h2>
+      <div class="label">
+        <a href="">CSS</a>
+      </div>
+      <div class="content">
+        内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容
+      </div>
+      <p class="more">
+        <a href="">阅读全文</a>
+      </p>
+      <p class="date">发布于 2017-10-19 11:11</p>
+    </article>
+    <article class="article">
+      <h2 class="title">测试用标题2</h2>
+      <div class="label">
+        <a href="">HTML</a>
+      </div>
+      <div class="content">
+        内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容
+      </div>
+      <p class="more">
+        <a href="">阅读全文</a>
+      </p>
+      <p class="date">发布于 2017-10-19 12:12</p>
+    </article>
+    <div class="page">
+      <span class="pre">
+        <a href="">上一页</a>
+      </span>
+      <span class="next">
+        <a href="">下一页</a>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    data() {
+      return {
+        articleList: [],
+      }
+    },
+    components: {
+    },
+  }
+</script>
+
+<style lang="less">
+  .article-list {
+    width: 100%;
+    background: rgb(255, 255, 255);
+    border-radius: 10px;
+    margin: 0 auto;
+    padding: 20px;
+    box-shadow: 0 16px 24px 1px rgba(0, 0, 0, .14), 0 6px 50px 1px rgba(0, 0, 0, .12), 0 6px 10px -5px rgba(0, 0, 0, .2);
+    .article {
+      position: relative;
+      padding: 15px;
+      border-bottom: 1px solid #f3f5f7;
+      .title {
+        text-align: center;
+        height: 40px;
+        line-height: 40px;
+        font-size: 20px;
+        font-weight: 700;
+      }
+      .label {
+        position: relative;
+        display: inline-block;
+        left: -50px;
+        top: -30px;
+        padding: 10px 10px 10px 30px;
+        background: #97dffd;
+        border-radius: 0 8px 8px 0;
+        box-shadow: 0 2px 2px 0 rgba(0, 0, 0, .14), 0 3px 1px -2px rgba(0, 0, 0, .2), 0 1px 5px 0 rgba(0, 0, 0, .12);
+        a {
+          color: white;
+          &:after {
+            position: absolute;
+            content: "";
+            top: 100%;
+            left: 0;
+            border-top: 0 solid transparent;
+            border-right-width: 1em;
+            border-right-color: #47456d;
+            border-right-style: solid;
+            border-bottom: 1em solid transparent;
+            border-left: 0 solid transparent;
+            width: 0;
+            height: 0;
+          }
+        }
+      }
+      .content {}
+      .more {
+        margin-top: 20px;
+        text-align: center;
+        a {
+          display: inline-block;
+          padding: 5px;
+          border-radius: 5px;
+          color: white;
+          background: #97dffd;
+        }
+      }
+      .date {
+        text-align: right;
+        font-size: 12px;
+      }
+    }
+    .page {
+      padding: 15px;
+      position: relative;
+      .pre,
+      .next {
+        position: absolute;
+        a {
+          display: inline-block;
+          padding: 5px;
+          border-radius: 3px;
+          color: white;
+          background: #97dffd;
+        }
+      }
+      .pre {
+        left: 15px;
+      }
+      .next {
+        right: 15px;
+      }
+    }
+  }
+</style>
--- src/views/blog.vue
+++ src/views/blog.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="blog">
     <blog-header></blog-header>
     <div class="banner">
       <div class="title-container">
@@ -8,43 +8,124 @@
         </div>
       </div>
     </div>
+    <div class="main">
+      <article-list class="article-list"></article-list>
+      <div class="side">
+        <side-section>
+          <div slot="sidecontent">
+            <div class="desc">
+              <h3 class="name">Christopher</h3>
+              <p class="text">coder not good at code</p>
+            </div>
+            <div class="avatar">
+              <img src="../../static/images/avatar.jpg" alt="my avatar" class="image">
+            </div>
+            <div class="social">
+              <ul>
+                <li class="type"><img src="" alt=""></li>
+                <li class="type"><img src="" alt=""></li>
+                <li class="type"><img src="" alt=""></li>
+                <li class="type"><img src="" alt=""></li>
+              </ul>
+            </div>
+          </div>
+        </side-section>
+      </div>
+    </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
   import blogHeader from '@/components/header'
+  import articleList from '@/components/articleList'
+  import sideSection from '@/components/sideSection'
 
   export default {
     components: {
       blogHeader,
+      articleList,
+      sideSection,
     },
   }
 </script>
 
 <style lang="less">
-  .banner {
-    position: relative;
-    background: url('../../static/images/banner.jpg') center no-repeat;
-    background-size: cover;
-    height: 260px;
-    .title-container {
+  .blog {
+    .banner {
       position: relative;
-      top: 40px;
-      width: 80%;
-      padding: 50px;
-      margin: 0 auto;
-      .title {
-        float: left;
-        h1 {
-          font-size: 40px;
-          font-family: sans-serif;
-          text-align: left;
-          text-shadow: 0 2px #f3f5f7;
-          color: rgba(255, 255, 255, 0.8);
+      background: url('../../static/images/banner.jpg') center no-repeat;
+      background-size: cover;
+      height: 260px;
+      .title-container {
+        position: relative;
+        top: 40px;
+        width: 80%;
+        padding: 50px;
+        margin: 0 auto;
+        .title {
+          float: left;
+          h1 {
+            font-size: 40px;
+            font-family: sans-serif;
+            text-align: left;
+            text-shadow: 0 2px #f3f5f7;
+            color: rgba(255, 255, 255, 0.8);
+          }
+          &:after {
+            clear: both;
+            content: '';
+          }
         }
-        &:after {
-          clear: both;
-          content: '';
+      }
+    }
+    .main {
+      position: relative;
+      width: 80%;
+      margin: 0 auto;
+      margin-top: -30px;
+      @media (max-width: 800px) {
+        .article-list {
+          width: 100%;
+        }
+        .side {
+          display: none;
+          width: 0;
+        }
+      }
+      .article-list {
+        display: inline-block;
+        width: 60%;
+        transition: width .5s;
+        margin-right: 3%;
+      }
+      .side {
+        display: inline-block;
+        vertical-align: top;
+        padding-top: 50px;
+        width: 30%;
+        .section {
+          .desc {
+            margin-bottom: 15px;
+            .name {
+              height: 30px;
+              line-height: 30px;
+              font-size: 20px;
+              font-weight: 700;
+            }
+            .text {
+              padding-left: 10px;
+              font-size: 14px;
+              color: rgba(49, 53, 48, 0.5);
+            }
+          }
+          .avatar {
+            padding: 10px;
+            .image {
+              width: 200px;
+              height: 200px;
+              border-radius: 50%;
+            }
+          }
         }
       }
     }
